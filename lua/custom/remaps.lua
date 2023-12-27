b9607c9742ff6876058ab2196ad1f9a21f3f3f17
Adelino Neto <adelino.neto@showtecnologiacom>
2023-12-27 14:22:31 -0300

    Added telescope-file-browser

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set('n', '<Leader>pv', ':Ex<CR>', { desc = 'Explore' })
 
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Find Files' })
 
+-- Open telescope-file-browser when pressing Ctrl + B
+vim.api.nvim_set_keymap("n", '<C-b>', ":Telescope file_browser<CR>", { noremap = true })

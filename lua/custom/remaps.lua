801c39319a9e0188afbc8e00759155ae9f12d880
Adelino Neto <adelino.neto@showtecnologiacom>
2023-12-27 14:14:12 -0300

    Added new remaps for 'leader' and ':Ex' command

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -1,2 +1,8 @@
+-- Leader key: <Space>
+vim.g.mapleader = ' ';
+
+-- Remap :Ex to Leader + pv
+vim.keymap.set('n', '<Leader>pv', ':Ex<CR>', { desc = 'Explore' })
+
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Find Files' })
 

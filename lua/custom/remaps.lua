774e020d1d02d30a5502234d25719b2ef9776e5b
Adelino Neto <adelino.neto@showtecnologiacom>
2023-12-28 17:45:38 -0300

    Changed Telescope find_files() to git_files() when Ctrl+P

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -4,7 +4,7 @@ vim.g.mapleader = ' ';
 -- Remap :Ex to Leader + pv
 vim.keymap.set('n', '<Leader>pv', ':Ex<CR>', { desc = 'Explore' })
 
-vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Find Files' })
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Find Files' })
 
 -- Open telescope-file-browser when pressing Ctrl + B
 vim.api.nvim_set_keymap("n", '<C-b>', ":Telescope file_browser<CR>", { noremap = true })
